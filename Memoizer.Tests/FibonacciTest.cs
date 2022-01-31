7a9effc56dfd43017cd5dfdbb3df4e022adb38e6
Gareth Williams <github@randomsyntax.net>
2022-01-31 11:03:15 +0000

    Github Nuget workflow
    Added github nuget release workflow

--- Memoizer.Tests/FibonacciTest.cs
+++ Memoizer.Tests/FibonacciTest.cs
@@ -21,23 +21,24 @@ namespace Memoizer.Tests
             var expected = 13;
 
             // ACT 
-            var res = Fibonacci(n);
+            var actual = Fibonacci(n);
 
             // ASSERT
-            res.Should().Be(expected);
+            actual.Should().Be(expected);
         }
 
         [Theory]
         [InlineData(100, "354224848179261915075")]
         [InlineData(200, "280571172992510140037611932413038677189525")]
         [InlineData(300, "222232244629420445529739893461909967206666939096499764990979600")]
-        public void Fibonacci__Should_Return_Expected_When_N_Is_Large(int n, string expected)
+        public void Fibonacci__Should_Return_Expected_When_N_Is_Large(int n, string shouldBe)
         {
             // ACT 
-            var res = Fibonacci(n);
+            var actual = Fibonacci(n);
+            var expected = BigInteger.Parse(shouldBe);
 
             // ASSERT
-            res.Should().Be(BigInteger.Parse(expected));
+            actual.Should().Be(expected);
         }
     }
 }
